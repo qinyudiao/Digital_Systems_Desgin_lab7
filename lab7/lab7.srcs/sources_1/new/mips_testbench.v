70d94fa0a5eadca77b7e9d556231265ade0eb00b
Unknown <audreygan@utexas.edu>
2019-04-26 15:28:53 -0500

    added testbench code from book (fig 9-10)

--- lab7/lab7.srcs/sources_1/new/mips_testbench.v
+++ lab7/lab7.srcs/sources_1/new/mips_testbench.v
@@ -1,72 +1,67 @@
 module mips_testbench ();
-  reg CLK;
-  reg RST;
-  wire CS;
-  wire WE;
-  wire [31:0] Mem_Bus;
-  wire [6:0] Address;
+    reg CLK;
+    reg RST;
+    wire CS, CS_Mux;
+    wire WE, WE_Mux;
+    wire [31:0] Mem_Bus;
+    wire [6:0] Address, Address_Mux, AddressTB;
 
-  initial
-  begin
-    CLK = 0;
-  end
-
-  MIPS CPU(CLK, RST, CS, WE, Address, Mem_Bus);
-  Memory MEM(CS, WE, CLK, Address, Mem_Bus);
-
-  always
-  begin
-    #5 CLK = !CLK;
-  end
-
-  always
-  begin
-    RST <= 1'b1; //reset the processor
-
-    //Notice that the memory is initialize in the in the memory module not here
-
-    @(posedge CLK);
-    // driving reset low here puts processor in normal operating mode
-    RST = 1'b0;
-
-    /* add your testing code here */
-    // you can add in a 'Halt' signal here as well to test Halt operation
-    // you will be verifying your program operation using the
-    // waveform viewer and/or self-checking operations
+    parameter N = 10;
+    reg [31:0] expected [N:1];
+    reg init, WE_TB, CS_TB;
     
-//    start:
-//        addi $6, $1, 0
-//        andi $8, $8, 0
-//        lui $4, 28672
-//        lui $5, 32767
-//        ori $8, $8, 11
+    integer i;
+    
+    initial begin
+        expected[1] = 32'h00000006; //$1 content=6 decimal
+        expected[2] = 32'h00000012; //$2 content=18 decimal
+        expected[3] = 32'h00000018; //$3 content=24 decimal
+        expected[4] = 32'h0000000C; //$4 content=12 decimal
+        expected[5] = 32'h00000002; //$5 content=2
+        expected[6] = 32'h00000016; //$6 content=22 decimal
+        expected[7] = 32'h00000001; //$7 content=1
+        expected[8] = 32'h00000120; //$8 content=288 decimal
+        expected[9] = 32'h00000003; //$9 content=3
+        expected[10] = 32'h00412022; //$10 content=5th instr
+        CLK = 0;
+    end
+    
+    MIPS CPU(CLK, RST, CS, WE, Address, Mem_Bus);
+    Memory MEM(CS_Mux, WE_Mux, CLK, Address_Mux, Mem_Bus);
+    
+    assign Address_Mux = (init)? AddressTB : Address;
+    assign WE_Mux = (init)? WE_TB : WE;
+    assign CS_Mux = (init)? CS_TB : CS;
+    
+    always
+        begin
+        #10 CLK = ~CLK;
+        end
+    
+    always begin
+        RST <= 1'b1; //reset the processor
+        //Notice that the memory is initialize in the in the memory module not here
+        @(posedge CLK); //wait until posedge CLK
+        //initialize the instructions from the testbench
+        init <= 1; CS_TB <= 1; WE_TB <= 1;
+        @(posedge CLK);
+        init <= 0; CS_TB <= 0; WE_TB <= 0;
+        // driving reset low here puts processor in normal operating mode
+        @(posedge CLK);
+            RST = 1'b0;
+            for(i = 1; i <= N; i = i+1)begin
+                @(posedge WE); //when a store word is executed
+                @(negedge CLK);
+                if(Mem_Bus != expected [i])
+                    $display("Output mismatch: got %d, expected %d", Mem_Bus, expected [i]);
+            end
         
-//    loop:
-//        beq $6, $1, loop
-//        addi $6, $1, 0
-//        sll $7, $8, $7
-//        jr $7
-//        j loop
+        /* add your testing code here */
+        // you can add in a 'Halt' signal here as well to test Halt operation
+        // you will be verifying your program operation using the
+        // waveform viewer and/or self-checking operations
         
-//    call_table:
-//        jal operation0
-//        j loop
-//        jal operation1
-//        j loop
-//        jal operation2
-//        j loop
-//        jal operation3
-//        j loop
-//        jal operation4
-//        j loop
-//        jal operation5
-//        j loop
-        
-//    operation0:
-//        add8 $2, $4, $5
-
-    $display("TEST COMPLETE");
-    $stop;
-  end
-
+            $display("TEST COMPLETE");
+            $stop;
+        end
 endmodule
